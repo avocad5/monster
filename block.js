4f8a4dc9d07cdebcf350f5243576a93d894c0f8b
avocad5 <63831073+avocad5@users.noreply.github.com>
2020-07-07 18:20:10 +0900

    Update block.js

--- block.js
+++ block.js
@@ -1,7 +1,6 @@
 function getblockid(name){
- var block = _.find(Entry.variableContainer.functions_,
-        d => d.block.template.replace(/ /gi,'') == name);
- return block.id
+ return _.find(Entry.variableContainer.functions_,
+        d => d.block.template.replace(/ /gi,'') == name).id
 }
 
 Entry.block[`func_${getblockid('%1경고하기')}`].paramsKeyMap = { content: 0 };
