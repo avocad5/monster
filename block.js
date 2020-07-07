81201bc55b80fac9b0f65dfbe8881ff606be8032
avocad5 <63831073+avocad5@users.noreply.github.com>
2020-07-07 18:20:49 +0900

    Update block.js

--- block.js
+++ block.js
@@ -3,7 +3,7 @@ function getblockid(name){
         d => d.block.template.replace(/ /gi,'') == name).id
 }
 
-Entry.block[`func_${getblockid('%1경고하기')}`].paramsKeyMap = { content: 0 };
-Entry.block[`func_${getblockid('%1경고하기')}`].func = (sprite, script) => {
+Entry.block[`func_${getblockid('%1경고하기%2')}`].paramsKeyMap = { content: 0 };
+Entry.block[`func_${getblockid('%1경고하기%2')}`].func = (sprite, script) => {
     alert(script.getValue('content',script))
 }
