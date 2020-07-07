83db312e1f5a55533128a6af6631e93c74ef2955
avocad5 <63831073+avocad5@users.noreply.github.com>
2020-07-07 18:28:21 +0900

    Update block.js

--- block.js
+++ block.js
@@ -3,7 +3,26 @@ function getblockid(name){
         d => d.block.template.replace(/ /gi,'') == name).id
 }
 
+
+//alert 블록
 Entry.block[`func_${getblockid('%1경고하기%2')}`].paramsKeyMap = { content: 0 };
 Entry.block[`func_${getblockid('%1경고하기%2')}`].func = (sprite, script) => {
-    alert(script.getValue('content',script))
+  alert(script.getValue('content',script))
+}
+
+
+//comfirm 블록
+Entry.block[`func_${getblockid('%1확인하기%2')}`].paramsKeyMap = { content: 0 };
+Entry.block[`func_${getblockid('%1확인하기%2')}`].func = (sprite, script) => {
+  if(confirm(script.getValue('content',script))==true){
+   Entry.variableContainer.getVariableByName('avo').value_ = '확인'
+  } else{
+   Entry.variableContainer.getVariableByName('avo').value_ = '취소'
+}
+
+
+//prompt 블록
+Entry.block[`func_${getblockid('%1확인하기%2')}`].paramsKeyMap = { content: 0 };
+Entry.block[`func_${getblockid('%1확인하기%2')}`].func = (sprite, script) => {
+    Entry.variableContainer.getVariableByName('avo').value_ = prompt(script.getValue('content',script))
 }
