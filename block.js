482db2d497c11b4894dba23cae42d2e9adfd90ab
avocad5 <63831073+avocad5@users.noreply.github.com>
2020-07-13 17:31:40 +0900

    Update block.js

--- block.js
+++ block.js
@@ -31,8 +31,29 @@ Entry.block[`func_${getblockid('%1물어보기%2')}`].func = (sprite, script) =>
 }
 
 
-//toast 블록
+//toast 블록 (알림)
 Entry.block[`func_${getblockid('%1제목과%2내용의 알림창띄우기%3')}`].paramsKeyMap = { title : 0, content: 1 };
-Entry.block[`func_${getblockid('%1물어보기%2')}`].func = (sprite, script) => {
+Entry.block[`func_${getblockid('%1제목과%2내용의 알림창띄우기%3')}`].func = (sprite, script) => {
     Entry.toast['success'](script.getValue('title',script),script.getValue('content',script),false)
+}
+
+
+//toast 블록 (경고)
+Entry.block[`func_${getblockid('%1제목과%2내용의 경고창띄우기%3')}`].paramsKeyMap = { title : 0, content: 1 };
+Entry.block[`func_${getblockid('%1제목과%2내용의 경고창띄우기%3')}`].func = (sprite, script) => {
+    Entry.toast['alert'](script.getValue('title',script),script.getValue('content',script),false)
+}
+
+
+//콘솔 log
+Entry.block[`func_${getblockid('콘솔창에%1라고쓰기%2')}`].paramsKeyMap = { content: 0 };
+Entry.block[`func_${getblockid('콘솔창에%1라고쓰기%2')}`].func = (sprite, script) => {
+  console.log(script.getValue('content',script))
+}
+
+
+//콘솔 clear
+Entry.block[`func_${getblockid('콘솔창 초기화하기%1')}`].paramsKeyMap = {};
+Entry.block[`func_${getblockid('콘솔창 초기화하기%1')}`].func = (sprite, script) => {
+  console.clear()
 }
